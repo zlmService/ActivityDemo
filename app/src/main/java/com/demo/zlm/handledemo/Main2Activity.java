a063aa69396a17a1d14f7137f4b77d431723ee0b
zmlService <zlm213599lz@gmail.com>
2016-03-22 22:25:38 +0800

    update

--- app/src/main/java/com/demo/zlm/handledemo/Main2Activity.java
+++ app/src/main/java/com/demo/zlm/handledemo/Main2Activity.java
@@ -20,11 +20,7 @@ public class Main2Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
                 Snackbar.make(view, "Replace with he own action", Snackbar.LENGTH_LONG)
-=======
-                Snackbar.make(view, "Replace with me own action", Snackbar.LENGTH_LONG)
->>>>>>> 813b0bed8af40bab854f7ecac4d5dd81915f2cc6
                         .setAction("Action", null).show();
             }
         });
